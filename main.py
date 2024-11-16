fc0db400b74e4145ee1df993a7834adf03bdab77
selimakar <iletisimselimakar@gmail.com>
2024-11-17 00:08:42 +0300

    threading is completed

--- main.py
+++ main.py
@@ -56,3 +56,28 @@ urls = ['https://postman-echo.com/delay/3'] * 10
 
 # Get data requests with threading
 
+class ThreadingDownloader(threading.Thread):
+    json_array = []
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+    def run(self):
+        response = requests.get(self.url)
+        self.json_array.append(response.json())
+        return self.json_array
+
+def get_data_threading(urls):
+    st = time.time()
+    threads = []
+    for url in urls:
+        t = ThreadingDownloader(url)
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+        print(t)
+    et = time.time()
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
+get_data_threading(urls) # 4 seconds
